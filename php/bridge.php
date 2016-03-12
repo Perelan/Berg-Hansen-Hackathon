e32fbbe59183b52203659b5d3484a7a26e5086d7
Sharu K <sharanak@uio.no>
2016-03-12 10:56:50 +0100

    FINISH

---
+++ php/bridge.php
@@ -0,0 +1,57 @@
+<?php
+
+$apiKey = base64_encode("V1:p542inzssp1am6et:DEVCENTER:EXT"); // Insert apiKey here
+$apiSecret = base64_encode("RsmVI5y7"); // Insert apiSecret here
+$tokenKey = base64_encode($apiKey.":".$apiSecret);
+
+function getAuthToken(){
+  global $tokenKey;
+
+  $current_time = time();
+  // Validate if expire timestamp exists or if the token has expired (then renew the token);
+  if($_SESSION['expire_ts'] < $current_time) {
+
+  	$ch = curl_init("https://api.test.sabre.com/v2/auth/token");
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER,array('Authorization: Basic ' . $tokenKey, 'Content-Type: application/x-www-form-urlencoded'));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$json = curl_exec($ch);
+		curl_close($ch);
+
+  	$session = json_decode($json, true);
+
+  	$_SESSION['access_token'] = $session['access_token'];
+  	$_SESSION['token_type'] = $session['token_type'];
+  	$_SESSION['expires_in'] = $session['expires_in'];
+  	$_SESSION['expire_ts'] = 1200 + time();
+
+  }
+}
+
+function sendRequest($payload, $data){
+  global $tokenKey;
+  $retVal = 'null';
+  if(!isset($_SESSION['expire_ts']) || $_SESSION['expire_ts'] < $current_time){
+    //try to get authentication token
+    getAuthToken();
+  }
+
+  $ch = curl_init("https://api.test.sabre.com/" . $payload);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	if($data == null){
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/x-www-form-urlencoded;charset=UTF-8','Authorization:Bearer ' . $_SESSION['access_token']));
+	}else{
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json;charset=UTF-8','Transfer-Encoding: chunked','Authorization:Bearer ' . $_SESSION['access_token']));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	}
+
+	$json = curl_exec($ch);
+	curl_close($ch);
+
+	$session = json_decode($json, true);
+
+  return $session;
+}
---
+++ php/test.php
@@ -0,0 +1,139 @@
+<?php
+//if session is not started
+if(!session_id()){
+    session_start();
+}
+
+//get code for connecting and fetching data from API
+require_once('bridge.php');
+
+function sort_array($data){
+  usort($data, function($a, $b) {
+    return $a['Fare'] < $b['Fare'] ? -1 : 1;
+  });
+
+  return $data;
+}
+
+function add_car($data){
+
+    $buffer = '{
+                "OTA_VehAvailRateRQ": {
+                  "VehAvailRQCore": {
+                    "QueryType": "Shop",
+                    "VehRentalCore": {
+                      "PickUpDateTime": "' . $data['DepartureDateTime'] . '",
+                      "ReturnDateTime": "' . $data['ReturnDateTime'] . '",
+                      "PickUpLocation": {
+                        "LocationCode": "' . $data['DestinationLocation'] . '"
+                      }
+                    }
+                  }
+                }
+              }';
+
+
+    $session = sendRequest('v2.4.0/shop/cars', $buffer);
+
+    $session = $session['OTA_VehAvailRateRS']['VehAvailRSCore'];
+
+    $car = array(
+      "PickUpDateTime" => $session['VehRentalCore']['PickUpDateTime'],
+      "ReturnDateTime" => $session['VehRentalCore']['ReturnDateTime'],
+      "Fare" => $session['VehVendorAvails']['VehVendorAvail'][0]['VehAvailCore']['VehicleCharges']['VehicleCharge']['TotalCharge']['Amount']
+    );
+
+  return $car;
+}
+
+function create_client_data($data, $car){
+  $buffer = array();
+  $is_in = false;
+
+  $current_time =   $current_time = date("Y-m-d H:i:s", strtotime('+5 hours'));
+
+  foreach($data as $dest){
+    //if there is more than 5 hours to the flight
+    if(date("Y-m-d H:i:s", strtotime($dest['DepartureDateTime'])) > $current_time){
+      foreach ($buffer as $dest_inner){
+        if($dest_inner['DestinationLocation'] == $dest['DestinationLocation']){
+          $is_in = true;
+          break;
+        }
+      }
+
+      if(!$is_in){
+        $temp = array(
+          "DestinationLocation" => $dest['DestinationLocation'],
+          "DepartureDateTime" => $dest['DepartureDateTime'],
+          "ReturnDateTime" => $dest['ReturnDateTime'],
+          "Fare" => $dest['LowestFare']['Fare'],
+          "AirlineCodes" => $dest['LowestFare']['AirlineCodes'],
+          "Car" => null);
+          if($car == "true"){
+            $temp['Car'] = add_car($temp);
+          }
+
+        array_push($buffer, $temp);
+      }
+    }
+  }
+
+  return $buffer;
+}
+
+function overprise($data, $limit){
+  $buffer = array();
+
+  foreach ($data as $dest){
+    if(($dest['Fare'] + $dest['Car']['Fare']) < $limit && $dest['Car']['Fare'] != null){
+      array_push($buffer, $dest);
+    }
+  }
+
+  return $buffer;
+}
+
+function handle_client_flight($data){
+  $payload = 'origin=NYC';
+
+  if($data['departuredate'] != "null"){
+    $payload = $payload . "&departuredate=" . $data['departuredate'];
+  }
+  if($data['returndate'] != "null"){
+    $payload = $payload . "&returndate=" . $data['returndate'];
+  }
+  if($data['lengthofstay'] != "null"){
+    $payload = $payload . "&lengthofstay=" . $data['lengthofstay'];
+  }
+  if($data['maxfare'] != "null"){
+    $payload = $payload . "&minfare=0&maxfare=" . $data['maxfare'];
+  }else{
+    exit;
+  }
+
+  $payload = $payload . "&topdestinations=50";
+
+  $session = sendRequest('v2/shop/flights/fares?' . $payload, null);
+
+  $session = create_client_data($session['FareInfo'], $data['car']);
+
+  if($data['car'] == "true"){
+    $session = overprise($session, $data['maxfare']);
+  }
+
+  $session = sort_array($session);
+
+  echo json_encode($session, JSON_PRETTY_PRINT);
+
+}
+
+//get data from client
+$data_client = json_decode($_GET['data'], true);
+handle_client_flight($data_client);
+
+//echo json_encode(sort_array(create_client_data(handle_client_flight($data_client)['FareInfo'])), JSON_PRETTY_PRINT);
+
+//$data = sendRequest('v2/shop/flights/fares?origin=NYC&departuredate=&returndate=&lengthofstay=2&minfare=0&maxfare=1000&topdestinations=50');
+
+//$data = $data['FareInfo'];
